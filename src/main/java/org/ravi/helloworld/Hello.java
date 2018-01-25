cadc82b1b3c1171bdfaabfebc379f7abe8ea182e
kir94 <35758567+kir94@users.noreply.github.com>
2018-01-25 04:24:34 +0000

    Update Hello.java

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -5,9 +5,6 @@ public class Hello {
 	public String displayHelloMessage(String message){
 		message = "hi";
 		return message;
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.printl("hi");
 		
 		
 	}
